1479c56207614c88d2bf963faa6b81021dd8c5a9
charliewright <charles.wright@setter.com>
2019-12-16 15:13:14 -0800

    added ticket data so others can hop in

---
+++ src/data/tickets.js
@@ -0,0 +1,13 @@
+const ticket1 = {
+    id: 1,
+    jobType: "Plumbing diagnostic",
+    client: "Bruno Soares",
+    tier: "Tier 1" ,
+    setterProtect: true,
+    status: "Quoting",
+    awaiting: "Reply from client",
+    estimatedValue: "$150",
+    closingPercentage: "%90"
+  };
+  
+  export const tickets = [ticket1]
