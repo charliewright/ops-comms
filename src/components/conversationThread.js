f87416c3d9d5eaa6cc62a32e5df7909376cd9936
charliewright <charles.wright@setter.com>
2019-12-17 10:41:06 -0800

    send messages from either column

--- src/components/conversationThread.js
+++ src/components/conversationThread.js
@@ -1,14 +1,32 @@
 import React from "react";
-import {MessageWithTimeStamp} from './messageWithTimestamp'
+import { MessageWithTimeStamp } from "./messageWithTimestamp";
 import { EditableTextArea } from "./editableTextArea";
 
-export const ConversationThread = props => {
-  return (
-    <React.Fragment>
-      {props.thread.map(message => (
-        <MessageWithTimeStamp message={message}></MessageWithTimeStamp>
-      ))}
-      <EditableTextArea/>
-    </React.Fragment>
-  );
-};
+
+
+export class ConversationThread extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { thread: props.thread };
+  }
+
+  addMessage = messageContent => {
+    const fromOps = this.state.thread.slice(-1)[0]
+    fromOps.content= messageContent
+    fromOps.time = (new Date().toLocaleString()).slice(12) // time, rather than date
+    this.setState({thread: [fromOps].concat(this.state.thread)})
+  }
+
+  render = () => {
+    
+    return (
+      <React.Fragment>
+        {this.state.thread.map(message => (
+          <MessageWithTimeStamp message={message}></MessageWithTimeStamp>
+        ))}
+        <EditableTextArea sendMessage={this.addMessage} />
+      </React.Fragment>
+    );
+  };
+}
--- src/components/editableTextArea.js
+++ src/components/editableTextArea.js
@@ -1,17 +1,39 @@
 import React from "react";
-import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import { TextField } from "@material-ui/core";
 import { Button } from "@material-ui/core";
-import MailOutlineIcon from '@material-ui/icons/Email';
+import MailOutlineIcon from "@material-ui/icons/Email";
 
+export class EditableTextArea extends React.Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = { messageContent: "" };
+  }
 
-export const EditableTextArea = props => {
-  return (
-    <span>
-      <MailOutlineIcon style={{verticalAlign:"top"}}fontSize="large"/>
-      <TextareaAutosize rowsMin={5} style={{width: "90%"}}></TextareaAutosize>
-      <div style={{margin: "10px", textAlign: "center"}}>
-        <Button variant="contained" color="primary">Send</Button>
-      </div>
-    </span>
-  );
-};
+  textChanged = event => {
+    this.setState({messageContent: event.target.value})
+  }
+
+  sendMessage = messageContent => {
+    this.setState({messageContent: ""})
+    this.props.sendMessage(messageContent)
+  }
+
+  render = () => {
+    return (
+      <span>
+        <MailOutlineIcon style={{ verticalAlign: "top" }} fontSize="large" />
+        <TextField multiline value={this.state.messageContent} onChange={this.textChanged} rows={5} style={{ width: "90%" }}></TextField>
+        <div style={{ margin: "10px", textAlign: "center" }}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => this.sendMessage(this.state.messageContent)}
+          >
+            Send
+          </Button>
+        </div>
+      </span>
+    );
+  };
+}
