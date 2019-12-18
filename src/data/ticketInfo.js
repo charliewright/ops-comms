75cc05ff9bab0249ded131e8d593127fe4ca45f4
charliewright <charles.wright@setter.com>
2019-12-17 16:56:09 -0800

    add client info

--- src/data/ticketInfo.js
+++ src/data/ticketInfo.js
@@ -4,6 +4,13 @@ import { proConversation } from "./proConversation";
 import { clientConversation2 } from "./clientConversation2";
 import { proConversation2 } from "./proConversation2";
 
-
-export const ticketInfo = {0: {clientConversation, proConversation}, 2: {clientConversation: clientConversation2, proConversation: proConversation2}}
-
+export const ticketInfo = {
+  0: { clientConversation, proConversation },
+  1: { clientConversation, proConversation },
+  3: { clientConversation, proConversation },
+  4: { clientConversation, proConversation },
+  2: {
+    clientConversation: clientConversation2,
+    proConversation: proConversation2
+  }
+};
